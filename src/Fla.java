54cb8787bf03bbd1ffa07d85580b9b8b7830bc46
Suplo <ian.ramsianto@gmail.com>
2025-01-05 02:30:15 +0000

    second commit

---
+++ src/Fla.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FlappyBird extends JPanel implements ActionListener, KeyListener {
+    int boardWidth = 360;
+    int boardHeight = 640;
+
+    
+}
---
+++ src/FlappyBird.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FlappyBird extends JPanel implements Runnable {
