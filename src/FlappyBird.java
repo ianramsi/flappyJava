e9c32d240c6bc79bd8337a1e617f6355c1f3e700
Suplo Silpo <ian.ramsianto@gmail.com>
2025-01-05 16:49:08 +0700

    delete unintended file

--- src/FlappyBird.java
+++
@@ -1,7 +0,0 @@
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Random;
-
-public class FlappyBird extends JPanel implements Runnable {
